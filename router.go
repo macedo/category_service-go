7ca047a136cb6cfaab96d011661a0fec384bf6ac
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2015-09-20 23:39:27 -0300

    manage dependencies with godeps

--- router.go
+++ router.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/julienschmidt/httprouter"
+import "github.com/macedo/category_service-go/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
 
 func NewRouter() *httprouter.Router {
 	router := httprouter.New()
