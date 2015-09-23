fd7885348f45c1355df90d35eb6059f6c969d66c
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2015-09-23 19:32:53 -0300

    use env var to configure app

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/macedo/category_service-go/Godeps/_workspace/src/gopkg.in/caarlos0/env.v1"
 )
+
+var cfg config
+
+type config struct {
+	Port        string `env:"PORT" envDefault:"1987"`
+	DatabaseURL string `env:"DATABASE_URL" envDefault:"postgres://category_service-go:@127.0.0.1:5432/category_service-go?sslmode=disable"`
+}
+
+func init() {
+	cfg = config{}
+	env.Parse(&cfg)
+}
 
 func main() {
 	router := NewRouter()
-
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, router))
 }
